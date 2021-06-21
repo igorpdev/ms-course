c3e23a26981b34fa6ebf86d342f2d4ba2f7a0585
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-06-21 13:54:26 -0300

    Feign Client

--- hr-payroll/src/main/java/com/igorpdev/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/igorpdev/hrpayroll/services/PaymentService.java
@@ -1,30 +1,21 @@
 package com.igorpdev.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
 import com.igorpdev.hrpayroll.entities.Payment;
 import com.igorpdev.hrpayroll.entities.Worker;
+import com.igorpdev.hrpayroll.feignclients.WorkerFeignClient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentService {
 	
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 
 	public Payment getPayment(long workerId, int days) {
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", ""+workerId);
 		
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getNome(), worker.getDailyIncome(), days);
 	}
 }
