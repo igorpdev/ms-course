661742f80f74d5f71765ae205179d5e34aae1c4e
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-06-24 14:34:00 -0300

    UserFeignClient

---
+++ hr-oauth/src/main/java/com/igorpdev/hroauth/entities/Role.java
@@ -0,0 +1,35 @@
+package com.igorpdev.hroauth.entities;
+
+import java.io.Serializable;
+
+public class Role implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String roleName;
+
+    public Role() {
+    }
+
+    public Role(Long id, String roleName) {
+        this.id = id;
+        this.roleName = roleName;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return this.roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+}
