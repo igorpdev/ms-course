8d571947a87d2206ce8380c4c943ee0d576f0caa
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-06-29 13:01:34 -0300

    db config server

--- hr-worker/src/main/java/com/igorpdev/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/igorpdev/hrworker/resources/WorkerResource.java
@@ -8,7 +8,6 @@ import com.igorpdev.hrworker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
@@ -27,15 +26,15 @@ public class WorkerResource {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
-    @Value("${test.config}")
-    private String testConfig;
+    /* @Value("${test.config}")
+    private String testConfig; */
 
     @Autowired
     private WorkerRepository repository;
 
     @GetMapping(value = "/configs")
     public ResponseEntity<Void> getConfigs() {
-        logger.info("CONFIG = " + testConfig);
+        /* logger.info("CONFIG = " + testConfig); */
         return ResponseEntity.noContent().build();
     }
 
